b6e60ff175b12f7cf69c370161ba3eea474c1d73
Oluwanifemi <latunde.nifemi2016@gmail.com>
2025-08-07 20:43:06 +0100

    refactor: simplify Movies component layout by removing unnecessary div wrapper around Providers

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -2,10 +2,8 @@ import MoviesPage from '@/components/layout/Movies';
 import Providers from '@/components/providers';
 export default function Movies() {
   return (
-    <div>
-      <Providers requireAuth={true} redirectTo="/login">
-        <MoviesPage />
-      </Providers>
-    </div>
+    <Providers requireAuth={true} redirectTo="/login">
+      <MoviesPage />
+    </Providers>
   );
 }
