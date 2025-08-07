31024534c52bf5439ddc00f178d01d84e8a53de5
Oluwanifemi <latunde.nifemi2016@gmail.com>
2025-08-07 03:01:11 +0100

    refactor: replace ProtectedRoute with Providers component for improved authentication handling in Movies page

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,11 +1,12 @@
 import ProtectedRoute from '@/components/auth/ProtectedRoute';
 import MoviesPage from '@/components/layout/Movies';
+import Providers from '@/components/providers';
 export default function Movies() {
   return (
     <div>
-      <ProtectedRoute>
+      <Providers requireAuth={true} redirectTo="/login">
         <MoviesPage />
-      </ProtectedRoute>
+      </Providers>
     </div>
   );
 }
